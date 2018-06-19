b724f9f98c3386bae04e1b672ee9808c5d820af1
Thiago Maia da Costa <thiago.maia971@gmail.com>
2018-06-19 20:11:44 -0300

    Adjusts

--- AOP.NET/AOP.NET.Aspects/RepositoryFactory.cs
+++ AOP.NET/AOP.NET.Aspects/RepositoryFactory.cs
@@ -8,8 +8,8 @@ namespace AOP.NET.Aspects
         public static IRepository<T> Create<T>(IRepository<T> repo)
             => DynamicProxy<IRepository<T>>.Create(
                 repo,
-                (sender, info) => Console.WriteLine($"Before operation {info.Name}"),
-                (sender, info) => Console.WriteLine("After operation"),
-                (sender, info) => Console.WriteLine("Error"));
+                before: (sender, info) => Console.WriteLine($"Before operation {info.Name}"),
+                after: (sender, info) => Console.WriteLine("After operation"),
+                error: (sender, info) => Console.WriteLine("Error"));
     }
 }
